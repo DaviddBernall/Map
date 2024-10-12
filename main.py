eb8dfd13eae76ae1f25527c4183f10a47b38d855
DaviddBernall <100694003+DaviddBernall@users.noreply.github.com>
2024-10-12 10:38:07 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+import streamlit as st
+from data_processing import get_data
+from visualization import create_map, create_bar_chart
+from datetime import datetime, time
+
+# Crear un sidebar para seleccionar la variable a analizar
+opcion_analisis = st.sidebar.selectbox(
+    "Selecciona la variable a analizar:",
+    ("Número de Incidentes", "Prioridad")
+)
+
+# Filtro de fecha (rango de fechas)
+st.sidebar.markdown("### Filtro de fecha")
+start_date = st.sidebar.date_input("Fecha de inicio", value=datetime(2019, 1, 1))
+end_date = st.sidebar.date_input("Fecha de fin", value=datetime(2019, 12, 31))
+
+# Filtro de hora con un slider (rango de horas)
+st.sidebar.markdown("### Filtro de hora")
+start_time, end_time = st.sidebar.slider(
+    "Selecciona el rango de horas",
+    value=(time(0, 0), time(23, 59)),
+    format="HH:mm"
+)
+
+# Combinar fechas y horas seleccionadas
+start_datetime = datetime.combine(start_date, start_time)
+end_datetime = datetime.combine(end_date, end_time)
+
+# Obtener los datos con el filtro de fechas y horas
+df, geojson, color_var, title = get_data(opcion_analisis, start_datetime, end_datetime)
+
+# Verificar si el DataFrame tiene datos
+if df.empty:
+    st.warning("No se encontraron datos para el rango de fechas y horas seleccionado.")
+else:
+    # Mostrar mapa o gráfico en función de la opción seleccionada
+    if opcion_analisis == "Número de Incidentes":
+        fig = create_map(df, geojson, color_var, title)
+        st.plotly_chart(fig)
+    else:
+        fig = create_bar_chart(df, title)
+        st.plotly_chart(fig)
