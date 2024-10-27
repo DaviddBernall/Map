c5e4e4ae52ec687dac5a10cc702e12c79fa9a4be
DaviddBernall <100694003+DaviddBernall@users.noreply.github.com>
2024-10-27 09:58:25 -0500

    visualization.py

--- visualization.py
+++ visualization.py
@@ -2,18 +2,22 @@ import plotly.express as px
 
 # Función para crear el mapa coroplético
 def create_map(df, geojson, color_var, title):
-    fig = px.choropleth_mapbox(df, 
-                               geojson=geojson, 
-                               locations="LOCALIDAD", 
-                               featureidkey="properties.Nombre de la localidad", 
-                               color=color_var,  
-                               color_continuous_scale="Viridis", 
-                               center={"lat": 4.2910, "lon": -74.1221}, 
-                               mapbox_style="carto-positron", 
-                               zoom=8.6, 
-                               width=None,  # Ajustar automáticamente
-                               height=None,  # Ajustar automáticamente
-                               title=title)
+    fig = px.choropleth_mapbox(
+        df, 
+        geojson=geojson, 
+        locations="LOCALIDAD", 
+        featureidkey="properties.Nombre de la localidad", 
+        color=color_var,  
+        color_continuous_scale="Viridis", 
+        center={"lat": 4.2910, "lon": -74.1221}, 
+        mapbox_style="carto-positron", 
+        zoom=8.6, 
+        title=title
+    )
+    
+    # Formato para mostrar los valores completos sin 'k' en miles
+    fig.update_layout(coloraxis_colorbar=dict(tickformat=".0f"))  # .0f muestra los valores completos sin decimales
+    
     return fig
 
 # Función para crear el gráfico de barras
