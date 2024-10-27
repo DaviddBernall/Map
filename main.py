7f531e0baec0cff441b3ec117614c7d9ca6d62c4
DaviddBernall <100694003+DaviddBernall@users.noreply.github.com>
2024-10-27 09:52:53 -0500

    main.py

--- main.py
+++ main.py
@@ -15,31 +15,18 @@ opcion_analisis = st.sidebar.selectbox(
     ("Número de Incidentes", "Prioridad", "Tipo de Incidente")
 )
 
-# Filtro de fecha (rango de fechas)
-st.sidebar.markdown("### Filtro de fecha")
-start_date = st.sidebar.date_input("Fecha de inicio", value=datetime(2019, 1, 1), min_value=datetime(2019, 1, 1), max_value=datetime(2023, 12, 31))
-end_date = st.sidebar.date_input("Fecha de fin", value=datetime(2019, 12, 31), min_value=datetime(2019, 1, 1), max_value=datetime(2023, 12, 31))
-
-# Filtro de hora
-st.sidebar.markdown("### Filtro de hora")
-start_time, end_time = st.sidebar.slider(
-    "Selecciona el rango de horas",
-    value=(time(0, 0), time(23, 59)),
-    format="HH:mm"
-)
-
-# Combinar fechas y horas seleccionadas
-start_datetime = datetime.combine(start_date, start_time)
-end_datetime = datetime.combine(end_date, end_time)
+# Filtro de año
+st.sidebar.markdown("### Selecciona el año")
+year = st.sidebar.selectbox("Año", options=[2019, 2020, 2021, 2022, 2023])
 
 # Obtener los datos
-df, geojson, color_var, title = get_data(opcion_analisis, start_datetime, end_datetime)
+df, geojson, color_var, title = get_data(opcion_analisis, year)
 
 # Verificar si el DataFrame tiene datos
 if df.empty:
-    st.warning("No se encontraron datos para el rango de fechas y horas seleccionado.")
+    st.warning("No se encontraron datos para el año seleccionado.")
 else:
-    # Mostrar mapa, gráfico de barras, o treemap en función de la opción seleccionada
+    # Mostrar el gráfico en función de la opción seleccionada
     if opcion_analisis == "Número de Incidentes":
         fig = create_map(df, geojson, color_var, title)
         st.plotly_chart(fig, use_container_width=True)
