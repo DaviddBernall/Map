0ef88f5b1bb0b4572517afa326f41d16397a2857
DaviddBernall <100694003+DaviddBernall@users.noreply.github.com>
2024-10-27 11:38:08 -0500

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from data_processing import get_data
-from visualization import create_map, create_bar_chart, create_treemap, create_histogram, create_choropleth_map
+from visualization import create_map, create_bar_chart, create_treemap, create_histogram
 from datetime import datetime, time
 
 # Configuración de la página
@@ -37,17 +37,5 @@ else:
         fig = create_treemap(df, "Distribución de Incidentes por Tipo")
         st.plotly_chart(fig, use_container_width=True)
     elif opcion_analisis == "Edad":
-        df, geojson, color_var, title = get_data(opcion_analisis, year)
-
-        # Crear el mapa coroplético
-        choropleth_fig = create_choropleth_map(df, geojson, color_var, "Edad Promedio por Localidad")
-        
-        # Crear el histograma
-        histogram_fig = create_histogram(df, title)
-    
-        # Mostrar el mapa coroplético
-        st.plotly_chart(choropleth_fig)
-    
-        # Mostrar el histograma solo si se creó correctamente
-        if histogram_fig:
-            st.plotly_chart(histogram_fig)
+        fig = create_histogram(df, title)
+        st.plotly_chart(fig, use_container_width=True)
