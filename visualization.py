4aad088a09d2ed6bc397efe4407b392f834d6030
DaviddBernall <100694003+DaviddBernall@users.noreply.github.com>
2024-11-15 19:09:44 -0500

    visualization.py

--- visualization.py
+++ visualization.py
@@ -2,14 +2,14 @@ import plotly.express as px
 import streamlit as st
 
 # Función para crear el mapa coroplético
-def create_map(df, geojson, color_var, title):
+def create_map(df, geojson, color_var, title,color):
     fig = px.choropleth_mapbox(
         df, 
         geojson=geojson, 
         locations="LOCALIDAD", 
         featureidkey="properties.Nombre de la localidad", 
         color=color_var,  
-        color_continuous_scale="Blues", 
+        color_continuous_scale=color, 
         center={"lat": 4.2910, "lon": -74.1221}, 
         mapbox_style="carto-positron", 
         zoom=8.6, 
@@ -55,7 +55,11 @@ def create_treemap(df, title):
     return fig
 
 def create_histogram(df, title):
-    fig = px.histogram(df, x='EDAD', title=title, nbins=20)
+    fig = px.histogram(
+        df, 
+        x='EDAD', 
+        title=title, 
+        nbins=20)
 
     # Agregar borde a cada barra
     fig.update_traces(marker=dict(line=dict(color='black', width=1)))  # Color y ancho del borde
@@ -65,6 +69,35 @@ def create_histogram(df, title):
         xaxis_title='Edad',
         yaxis_title='Número de Incidentes',
         title=title
+    )
+
+    return fig
+
+def create_segmented_bar_chart(df, title):
+    if df.empty:
+        raise ValueError("El DataFrame está vacío. No se puede crear el gráfico.")
+
+    # Crear gráfico con Plotly Express
+    fig = px.bar(
+        df,
+        x="YEAR",
+        y="count",
+        color="GENERO",
+        facet_col="TIPO_INCIDENTE",
+        title=title,
+        labels={"count": "Cantidad de Incidentes", "YEAR": "Año", "GENERO": "Género"},
+        barmode="stack",  # Barras segmentadas
+        text="count"      # Mostrar los valores
+    )
+
+    # Actualizar diseño
+    fig.update_traces(textposition='inside')
+    fig.update_layout(
+        legend_title="Género",
+        xaxis=dict(title="Año"),
+        yaxis=dict(title="Cantidad de Incidentes"),
+        margin=dict(t=50, b=50, l=50, r=50),
+        height=600,
     )
 
     return fig
